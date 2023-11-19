726320bb9b89240f9b96dcf0d3addf076b1ee25a
fabio vieira <fabio.hargreaves@goeyewear.com.br>
2023-11-19 18:19:57 -0300

    finishing sqs implementation

--- broker/broker.go
+++ broker/broker.go
@@ -3,21 +3,21 @@ package broker
 import "context"
 
 type MessageBroker interface {
-	SendMessage(ctx context.Context, message Message, topic string) error
-	ReceiveMessages(ctx context.Context, topic string) (*Message, error)
-	Ack(ctx context.Context) error
+	SendMessage(ctx context.Context, message *Message, topic string) error
+	ReceiveMessages(ctx context.Context, maxMessages int64, topic string) (*Message, error)
+	Ack(ctx context.Context, message *Message, topic string) error
 }
 
 type NullBroker struct{}
 
-func (n *NullBroker) SendMessage(ctx context.Context, message Message, topic string) error {
+func (n *NullBroker) SendMessage(_ context.Context, _ *Message, _ string) error {
 	return nil
 }
 
-func (n *NullBroker) ReceiveMessages(ctx context.Context, topic string) (*Message, error) {
+func (n *NullBroker) ReceiveMessages(_ context.Context, _ int64, _ string) (*Message, error) {
 	return nil, nil
 }
 
-func (n *NullBroker) Ack() error {
+func (n *NullBroker) Ack(_ context.Context, _ *Message, _ string) error {
 	return nil
 }
--- broker/sqs/sqs.go
+++ broker/sqs/sqs.go
@@ -2,9 +2,11 @@ package sqs
 
 import (
 	"context"
+	"errors"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 	"github.com/fabiohvieira/broker/broker"
 )
 
@@ -21,13 +23,21 @@ func New(client Client) *Broker {
 type Client interface {
 	SendMessage(ctx context.Context, params *sqs.SendMessageInput, optFns ...func(*sqs.Options)) (*sqs.SendMessageOutput, error)
 	ReceiveMessage(ctx context.Context, params *sqs.ReceiveMessageInput, optFns ...func(*sqs.Options)) (*sqs.ReceiveMessageOutput, error)
-	Ack(ctx context.Context) error
+	DeleteMessage(ctx context.Context, params *sqs.DeleteMessageInput, optFns ...func(*sqs.Options)) (*sqs.DeleteMessageOutput, error)
 }
 
 func (b *Broker) SendMessage(ctx context.Context, message broker.Message, topic string) error {
 	params := &sqs.SendMessageInput{
-		MessageBody: aws.String(string(message.Body)),
-		QueueUrl:    aws.String(topic),
+		MessageBody:       aws.String(string(message.Body)),
+		QueueUrl:          aws.String(topic),
+		MessageAttributes: map[string]types.MessageAttributeValue{},
+	}
+
+	for key, value := range message.Header {
+		params.MessageAttributes[key] = types.MessageAttributeValue{
+			DataType:    aws.String("String"),
+			StringValue: aws.String(value),
+		}
 	}
 
 	_, err := b.client.SendMessage(ctx, params)
@@ -38,9 +48,10 @@ func (b *Broker) SendMessage(ctx context.Context, message broker.Message, topic 
 	return nil
 }
 
-func (b *Broker) ReceiveMessages(ctx context.Context, topic string) ([]broker.Message, error) {
+func (b *Broker) ReceiveMessages(ctx context.Context, maxMessages int64, topic string) ([]broker.Message, error) {
 	params := &sqs.ReceiveMessageInput{
-		QueueUrl: aws.String(topic),
+		QueueUrl:            aws.String(topic),
+		MaxNumberOfMessages: int32(maxMessages),
 	}
 
 	output, err := b.client.ReceiveMessage(ctx, params)
@@ -59,8 +70,23 @@ func (b *Broker) ReceiveMessages(ctx context.Context, topic string) ([]broker.Me
 	return messages, nil
 }
 
-func (b *Broker) Ack(ctx context.Context) error {
-	return b.client.Ack(ctx)
+func (b *Broker) Ack(ctx context.Context, message *broker.Message, topic string) error {
+	h, ok := message.Header["ReceiptHandle"]
+	if !ok {
+		return errors.New("ReceiptHandle not found")
+	}
+
+	params := &sqs.DeleteMessageInput{
+		QueueUrl:      aws.String(topic),
+		ReceiptHandle: &h,
+	}
+
+	_, err := b.client.DeleteMessage(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func NewSQSClient(ctx context.Context) (*sqs.Client, error) {
